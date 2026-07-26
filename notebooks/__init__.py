44ceff91ee55ca870b6b1fb9b56930f4004feae0
hsdsp9-ship-it <hsdsp9@gmail.com>
2026-07-26 17:14:00 +0530

    Make notebooks a package for tests

---
+++ notebooks/__init__.py
@@ -0,0 +1,5 @@
+"""Notebooks package init to allow tests to import notebook helpers.
+This file intentionally left minimal so tests and CI can `import notebooks`.
+"""
+
+__all__ = ["config", "logging_helper"]
